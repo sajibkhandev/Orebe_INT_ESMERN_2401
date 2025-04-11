e6923e1ec0ef7155cb4269a69ea5190ded7e159d
sajibkhandev <sajibkhan.mern@gmail.com>
2025-04-11 15:48:14 +0600

    dom add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Shop from './pages/Shop';
+import Product from './pages/Product';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/about" element={<About />}></Route>
+    <Route path="/shop" element={<Shop />}></Route>
+    <Route path="/shop" element={<Shop />}></Route>
+    <Route path="/product" element={<Product />}></Route>
+    <Route path="/contact" element={<Contact />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/signup" element={<SignUp />}></Route>
+    <Route path="/cart" element={<Cart />}></Route>
+    <Route path="/checkout" element={<Checkout />}></Route>
+    <Route path="/*" element={<Error />}></Route>
+    </Route>
+  )
+);
+
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-red-500'>This is heading one tag</h1>
-    </div>
+    <RouterProvider router={router} />
+    
   )
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <button className='bg-red-500 py-2 px-5 text-white text-base rounded'>about</button>
+    </div>
+  )
+}
+
+export default About
